989a68264a128edbb7aebbe2cc6c547772907b59
Philipp Kitzberger <coding@kitze.net>
2024-11-16 14:57:12 +0100

    Set title attribute

--- resources/views/wine/index.blade.php
+++ resources/views/wine/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Wine list
+            Wine Wiz v0.0.2
         </h2>
     </x-slot>
 
