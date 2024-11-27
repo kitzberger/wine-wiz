3f6a7a48b6f794fc69e327dbaa08b0576f0fa8c9
Philipp Kitzberger <coding@kitze.net>
2024-11-27 16:00:55 +0100

    Remove remote fonts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,9 +8,6 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
         <link rel= "stylesheet" href= "https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" >
 
         <!-- Scripts -->
@@ -22,7 +19,7 @@
             <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
                 <div class="container">
                     <div class="navbar-brand">
-                        {{ __('Wine Wiz') }}
+                        <a href="{{ route('root') }}" class="text-default">{{ __('Wine Wiz') }}</a>
                     </div>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
